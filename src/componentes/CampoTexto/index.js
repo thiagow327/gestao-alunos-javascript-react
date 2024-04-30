11ae89e98a185ae05e00d50ddffcf7fd7eca9d2e
thiago.silva <thiagosilva327@live.com>
2024-04-30 18:08:40 -0300

    feat: "adiciona campo texto ao formulario"

---
+++ src/componentes/CampoTexto/index.js
@@ -0,0 +1,12 @@
+import "./CampoTexto.css"
+
+const CampoTexto = () => {
+    return (
+        <div className="campo-texto">
+            <label>Nome</label>
+            <input placeholder="Digite o nome do aluno"/>
+        </div>
+    )
+}
+
+export default CampoTexto
