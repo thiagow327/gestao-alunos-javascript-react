c7a098dbc9143d1e6eb98a31d20d8510718a4498
thiago.silva <thiagosilva327@live.com>
2024-04-30 20:43:57 -0300

    feat: "divide formulario em dois blocos css"

--- src/componentes/Formulario/index.js
+++ src/componentes/Formulario/index.js
@@ -26,21 +26,24 @@ const Formulario = () => {
         <section className="formulario">
             <form>
                 <h2>Preencha os dados para cadastrar um novo aluno</h2>
-                <CampoTexto label="Nome" placeholder="Digite o nome do aluno" />
-                <CampoTexto label="Idade" placeholder="Insira a idade do aluno" />
-                <CampoTexto label="Observação de Saúde" placeholder="Digite aqui qualquer observação de saúde da criança, como asma, autismo, epilepsia, etc" />
-                <CampoTexto label="Endereço" placeholder="Digite o endereço do aluno" />
+                <div className="bloco-esquerda">
+                    <CampoTexto label="Nome" placeholder="Digite o nome do aluno" />
+                    <CampoTexto label="Idade" placeholder="Insira a idade do aluno" />
+                    <CampoTexto label="Observação de Saúde" placeholder="Digite aqui qualquer observação de saúde da criança, como asma, autismo, epilepsia, etc" />
+                    <CampoTexto label="Endereço" placeholder="Digite o endereço do aluno" />
+                    <ListaSuspensa label="Escola" itens={escolas} />
+                    <ListaSuspensa label="Turno" itens={turnos} />
+                </div>
 
-                <ListaSuspensa label="Escola" itens={escolas} />
-                <ListaSuspensa label="Turno" itens={turnos} />
-
-                <CampoTexto label="Nome do Primeiro Responsável" placeholder="Digite o nome do primeiro responsável" />
-                <CampoTexto label="Celular do Primeiro Responsável" placeholder="Digite o celular do primeiro responsável" />
-                <CampoTexto label="Nome do Segundo Responsável" placeholder="Digite o nome do segundo responsável" />
-                <CampoTexto label="Celular do Segundo Responsável" placeholder="Digite o celular do segundo responsável" />
-                <CampoTexto label="Valor da Mensalidade" placeholder="Insira o valor da mensalidade" />
-
-                <Botao texto="Criar Aluno"/>
+                <div className="bloco-direita">
+                    <CampoTexto label="Nome do Primeiro Responsável" placeholder="Digite o nome do primeiro responsável" />
+                    <CampoTexto label="Celular do Primeiro Responsável" placeholder="Digite o celular do primeiro responsável" />
+                    <CampoTexto label="Nome do Segundo Responsável" placeholder="Digite o nome do segundo responsável" />
+                    <CampoTexto label="Celular do Segundo Responsável" placeholder="Digite o celular do segundo responsável" />
+                    <CampoTexto label="Valor da Mensalidade" placeholder="Insira o valor da mensalidade" />
+                <Botao texto="Criar Aluno" />
+                </div>
+                
             </form>
         </section>
     )
